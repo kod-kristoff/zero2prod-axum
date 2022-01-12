10931ba5f30a155a82f4cd203bbf3ee0d7d4b553
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-01-12 19:22:10 +0100

    test: add subscribe tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,6 +21,64 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let ctx = Context::try_new()
+        .await
+        .expect("Failed to spawn app");
+
+    let client = reqwest::Client::new();
+
+    // Act 
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("http://{}/subscriptions", ctx.addr)) 
+        .header("Content-Type", "application/x-www-form-urlencoded") 
+        .body(body) 
+        .send() 
+        .await 
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio:: test] 
+async fn subscribe_returns_a_400_when_data_is_missing() { 
+    // Arrange 
+    let ctx = Context::try_new()
+        .await
+        .expect("Failed to spawn app");
+
+    let client = reqwest::Client:: new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email") 
+    ];
+    for (invalid_body, error_message) in test_cases { 
+        // Act 
+        let response = client 
+            .post(&format!("http://{}/subscriptions", ctx.addr)) 
+            .header("Content-Type", "application/x-www-form-urlencoded") 
+            .body(invalid_body) 
+            .send() 
+            .await 
+            .expect("Failed to execute request.");
+
+        // Assert 
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.", 
+            error_message 
+        );
+    } 
+}
+
 struct Context {
     addr: SocketAddr,
 }
