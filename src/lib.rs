fdb25177b42805d703e60955562e06adfdf1a1f4
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-01-12 19:22:37 +0100

    stub: add subscribe stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpListener};
 
 use axum::{
+    extract::Form,
     Router,
 };
 use http::StatusCode;
@@ -9,11 +10,23 @@ async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
+#[derive(serde::Deserialize)]
+struct Subscribe {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: Form<Subscribe>) -> StatusCode {
+    StatusCode::OK
+}
+
 pub async fn run(listener: TcpListener) -> Result<(),std::io::Error> {
 
-    use axum::routing::get;
+    use axum::routing::{get, post};
     let app = Router::new()
-        .route("/health_check", get(health_check));
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe))
+        ;
     
     axum::Server::from_tcp(listener).unwrap()
         .serve(app.into_make_service())
