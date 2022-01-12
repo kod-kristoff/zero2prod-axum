a190834653413a6a255428c76eec44c717677d17
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-01-12 21:53:40 +0100

    build: add migration

---
+++ migrations/20220112192252_create_subscriptions_table.sql
@@ -0,0 +1,7 @@
+-- Create Subscriptions table
+CREATE TABLE subscriptions(
+    id BLOB PRIMARY KEY NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    subscribed_at DATETIME NOT NULL
+);
