2084d3e4057a8bf83106028956d6060e1e2fa8ba
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-01-23 15:09:32 +0100

    use postgres migration

--- migrations/20220115221800_create_subscriptions.sql
+++ migrations/20220115221800_create_subscriptions.sql
@@ -1,7 +1,7 @@
 -- Create Subscriptions table
 CREATE TABLE subscriptions(
-    id BLOB PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
     email TEXT NOT NULL UNIQUE,
     name TEXT NOT NULL,
-    subscribed_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    subscribed_at timestamptz NOT NULL
 );
